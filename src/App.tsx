a08e6f10d5f2cafeb920417dcb87bf7ffcda1811
San <warcommandar455@gmail.com>
2026-02-25 13:46:33 +0300

    80 percent simon game is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,59 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-
+const randomNumber = () => Math.floor(Math.random() * 4);
 function App() {
-  return <div>hi</div>;
+  const [colors, setColors] = useState(["green", "red", "yellow", "blue"]);
+  const [gussess, setGuesses] = useState<string[]>([colors[randomNumber()]]);
+  const [currentColor, setCurrentColor] = useState<string | null>(gussess[0]);
+  const [isMyTurn, setIsMyTurn] = useState<boolean>(false);
+  const [myGuesses, setMyGuesses] = useState<string[]>([]);
+
+  const handleClickColor = (color: string) => {
+    const newGuess = [...myGuesses, color];
+    if (newGuess.length === gussess.length) {
+      const isTrue = newGuess.every(
+        (myGuess, index) => myGuess === gussess[index],
+      );
+      if (isTrue) {
+        setGuesses([...gussess, colors[randomNumber()]]);
+
+        setIsMyTurn(false);
+        return;
+      }
+      setIsMyTurn(false);
+      setGuesses([gussess[gussess.length - 1]]);
+
+      alert("you lost");
+    }
+    setMyGuesses([...newGuess]);
+  };
+  useEffect(() => {
+    gussess.map((eachGuess, index) => {
+      setTimeout(() => {
+        if (index === gussess.length - 1) {
+          setIsMyTurn(true);
+          setCurrentColor(null);
+          return;
+        }
+
+        setCurrentColor(gussess[index + 1]);
+      }, 1000);
+    });
+  }, [gussess]);
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-2.5">
+      <h1>{isMyTurn ? "your turn" : "wait"}</h1>
+      <div className="grid grid-cols-2">
+        {colors.map((color) => (
+          <div
+            onClick={() => handleClickColor(color)}
+            style={{ backgroundColor: color }}
+            className={`w-30 h-30 ${!(currentColor === color) && "brightness-50"}`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
